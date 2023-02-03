cbc5b26e6de1995eb804d9d5931c3b823e53d5c7
Asem-Abdelhady <asemshawkey@gmail.com>
2023-02-03 19:17:41 +0300

    Fix: router-dom bug

--- frontend/src/pages/movies/Movies.tsx
+++ frontend/src/pages/movies/Movies.tsx
@@ -1,7 +1,24 @@
+import { Box, Center, Container } from "@chakra-ui/react";
 import React from "react";
 
 const Movies = () => {
-  return <div>Movies</div>;
+  return (
+    // <Container maxW="xl" centerContent>
+    //   <Box
+    //     display="flex"
+    //     justifyContent="center"
+    //     p={3}
+    //     bg="white"
+    //     w="100%"
+    //     m="40px 0 15px  0"
+    //     borderRadius="1g"
+    //     borderWidth="1px"
+    //   >
+    //     <Center>Filmus</Center>
+    //   </Box>
+    // </Container>
+    <div>yo</div>
+  );
 };
 
 export default Movies;
