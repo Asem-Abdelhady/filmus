463d27ab228c454536fe3d0ba3dcfd692605e8c9
Asem-Abdelhady <asemshawkey@gmail.com>
2023-04-22 11:01:20 +0300

    Add: profile page

--- src/tests/pages/Login.test.tsx
+++ src/tests/pages/Login.test.tsx
@@ -8,13 +8,13 @@ test("laods and displays login form", async () => {
   const setToken = jest.fn();
   const setUserId = jest.fn();
   render(<Login setToken={setToken} setUserId={jest.fn()} />);
-  // let userIdInput = screen.getAllByRole("textbox")[0];
+  let userIdInput = screen.getAllByRole("textbox")[0];
 
-  // let passwordInput = screen.getAllByRole("inputtextbox")[1];
-  // console.log(passwordInput);
-  // userEvent.type(userIdInput, "el3os10@gmail.com");
-  // userEvent.type(passwordInput, "password");
-  // await userEvent.click(screen.getByText("Login", { selector: "button" }));
-  // expect(setToken).toHaveBeenCalled();
-  // expect(setUserId).toHaveBeenCalled();
+  let passwordInput = screen.getAllByRole("inputtextbox")[1];
+  console.log(passwordInput);
+  userEvent.type(userIdInput, "el3os10@gmail.com");
+  userEvent.type(passwordInput, "password");
+  await userEvent.click(screen.getByText("Login", { selector: "button" }));
+  expect(setToken).toHaveBeenCalled();
+  expect(setUserId).toHaveBeenCalled();
 });
